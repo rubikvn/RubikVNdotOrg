3f236242c2dc285fd076efcfcc6025719bd6bd51
trungams <vttrung12@gmail.com>
2018-08-10 23:59:34 +0700

    Create installation options
    The script will install everything necessary for the project, excluding
    git, python3.6 and MySQL
    Add a new option to update the database only, this is useful for
    scheduled database update during production

--- install.sh
+++ install.sh
@@ -1,49 +1,90 @@
 #!/bin/bash
 
 # Install & set up virtualenv and project dependencies
-echo "Installing and setting up virtualenv..."
-sudo apt install python3.6-dev
-sudo apt install libmysqlclient-dev
-sudo python3.6 -m pip install virtualenv
-virtualenv rbvn-env/ -p python3.6
-source rbvn-env/bin/activate
-pip install -r requirements.txt
+first_time_install ()
+{
+  echo "Installing and setting up virtualenv..."
+  sudo apt install python3.6-dev, libmysqlclient-dev, pv
+  sudo python3.6 -m pip install virtualenv
+}
 
-echo -n "Your root user password for MySQL: "
-read -s MYSQL_PASSWORD
-echo
+initialize ()
+{
+  virtualenv rbvn-env/ -p python3.6
+  source rbvn-env/bin/activate
+  pip install -r requirements.txt
+}
 
-# Create mysql database named wca
-echo "Importing the database export..."
-echo "CREATE DATABASE IF NOT EXISTS wca; CREATE DATABASE IF NOT EXISTS rubikvn;" | mysql -u root --password=$MYSQL_PASSWORD
+db_setup ()
+{
+  echo -n "Your root user password for MySQL: "
+  read -s MYSQL_PASSWORD
+  echo
 
-# Create the database schema
-echo "Making migrations for Django project"
-python3.6 manage.py makemigrations
-python3.6 manage.py migrate
+  # Create mysql database named wca
+  echo "Importing the database export..."
+  echo "CREATE DATABASE IF NOT EXISTS wca; CREATE DATABASE IF NOT EXISTS rubikvn;" | mysql -u root --password=$MYSQL_PASSWORD
 
-# Download & import WCA database
-echo "Downloading WCA database export..."
-cd RubikVNdotOrg/db/
-if [ -e WCA_export.sql ]
+  # Create the database schema
+  echo "Making migrations for Django project"
+  python3.6 manage.py makemigrations
+  python3.6 manage.py migrate
+
+  # Download & import WCA database
+  echo "Downloading WCA database export..."
+  cd RubikVNdotOrg/db/
+  if [ -e WCA_export.sql ]
+  then
+    rm WCA_export.sql
+  fi
+  wget -q --show-progress https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip
+  unzip WCA_export.sql.zip
+
+  # Removing unnecessary files
+  rm README.txt
+  rm WCA_export.sql.zip
+
+  pv WCA_export.sql | cat | mysql -u root --password=$MYSQL_PASSWORD wca
+
+  # Extract from the database with our needed records
+  echo "Reading from database wca and creating new database rubikvn..."
+  pv vn_db_export.sql | cat | mysql -u root --password=$MYSQL_PASSWORD
+
+  echo "-------------------------------"
+  echo "Done setting up MySQL database."
+}
+
+finish ()
+{
+  # Escape virtualenv
+  deactivate
+  echo "Finished installation. You can now run the server using 'python3.6 manage.py runserver'."
+}
+
+usage ()
+{
+  echo "USAGE: ./install.sh [-u]"
+  echo "    ./install.sh        For first time installation"
+  echo "    ./install.sh -u     From second time installation, tell the script to update the database only"
+}
+
+# main script
+# Parsing arguments using getopts
+if [ $# -eq 0 ]
 then
-  rm WCA_export.sql
+  first_time_install
+  initialize
+  db_setup
+  finish
+else
+  while getopts ":u" opt; do
+    case ${opt} in
+      u )
+        db_setup
+        ;;
+      \?)
+        usage
+        ;;
+    esac
+  done
 fi
-wget -q --show-progress https://www.worldcubeassociation.org/results/misc/WCA_export.sql.zip
-unzip WCA_export.sql.zip
-
-# Removing unnecessary files
-rm README.txt
-rm WCA_export.sql.zip
-
-mysql -u root --password=$MYSQL_PASSWORD wca < WCA_export.sql
-
-# Extract from the database with our needed records
-echo "Reading from database wca and creating new database rubikvn..."
-mysql -u root --password=$MYSQL_PASSWORD < vn_db_export.sql
-
-echo "-------------------------------"
-echo "Done setting up MySQL database, you can now run the server."
-
-# Escape virtualenv
-deactivate
