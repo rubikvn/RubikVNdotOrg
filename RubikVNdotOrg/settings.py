6294ab014da8a535d2190dfa8d56eac52a6deb14
Trung Vuong Thien <vttrung12@gmail.com>
2018-09-29 22:54:00 +0000

    Add STATIC_ROOT in setting

--- RubikVNdotOrg/settings.py
+++ RubikVNdotOrg/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# STATIC_ROOT = "/home/trungams/rubikvn/static/"
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
