c624e050200355e80e862035e6c1680a60145651
trungams <vttrung12@gmail.com>
2018-11-13 13:31:38 -0500

    Move lib under RubikVNdotOrg

--- RubikVNdotOrg/lib/config/configs.py
+++ RubikVNdotOrg/lib/config/configs.py
@@ -3,8 +3,12 @@ import configparser
 class ServerConfig():
     def __init__(self):
         config = configparser.ConfigParser()
-        config.read('./RubikVNdotOrg/config/rubikvn.cnf')
+        config.read('./RubikVNdotOrg/lib/config/rubikvn.cnf')
 
         self.key = config['Secret']['key']
         self.db_uname = config['Database']['mysql_username']
         self.db_password = config['Database']['mysql_password']
+
+        self.oauth_client_id = config['OAuth2']['client_id']
+        self.oauth_client_secret = config['OAuth2']['client_secret']
+        self.oauth_callback_uri = config['OAuth2']['callback_uri']
---
+++ RubikVNdotOrg/lib/oauth_backend.py
@@ -0,0 +1,3 @@
+from RubikVNdotOrg.settings import server_configs
+
+# print(server_configs.oauth_client_id)
