535ea9e8e504ec86580d70d8147388cb92eb8fd7
Trung Vuong Thien <vttrung12@gmail.com>
2018-11-24 05:56:31 +0000

    Reconfigurations for static files
    In development, static files should go under static/
    When deploying, those files will be moved to static-collected/

--- RubikVNdotOrg/settings.py
+++ RubikVNdotOrg/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_ROOT = "/home/trungams/rubikvn/static/"
+STATIC_ROOT = "/home/trungams/rubikvn/static-collected/"
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static-collected/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
