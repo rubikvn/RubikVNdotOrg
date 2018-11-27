91bcd179ec319b821f7613b5ade1865c1acd0303
trungams <vttrung12@gmail.com>
2018-11-27 18:32:20 -0500

    Create JSON endpoint for retrieving rankings
    First attempt to resolve #4
    Add url routing and controller function
    Remove usage of django paginator until we have implemented a proper
    filter menu

--- apps/results/urls.py
+++ apps/results/urls.py
@@ -6,7 +6,6 @@ app_name = "results"
 urlpatterns = [
     path("", views.index, name="index"),
     path("ranking/", views.ranking, name="ranking"),
-
-    # path("ranks/", views.rank, name="rank"),
+    path("api/ranking", views.api_ranking, name="api_ranking"),
     # path("<slug:wca_id>/", views.person, name="person"),
 ]
