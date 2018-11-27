c85355cefc137c522295e234be04fbec6493c75a
trungams <vttrung12@gmail.com>
2018-11-26 20:58:45 -0500

    Temporary workaround to run server

--- apps/events/urls.py
+++ apps/events/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from . import views
 
 app_name = "events"
 urlpatterns = [
-
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
 ]
