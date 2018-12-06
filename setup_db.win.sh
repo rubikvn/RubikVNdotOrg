72c60ab407f1da5548bd3bd2a08475363e2a3e7a
Trung Vuong Thien <vttrung12@gmail.com>
2018-12-07 06:38:06 +0700

    Add database init file for mingw

---
+++ setup_db.win.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script to initialize MySQL database inside the container for development
+
+docker-compose up -d db
+
+# Copying template files to the database container
+docker cp RubikVNdotOrg\\db\\my.cnf rbvn_mysql_database:/root
+docker cp RubikVNdotOrg\\db\\rubikvn_schema.sql rbvn_mysql_database:/root
+
+docker-compose exec db bin/bash -c "cat /root/my.cnf | tee -a /etc/mysql/my.cnf"
+# Apply changes to config
+docker-compose restart db
+
+docker-compose exec db bin/bash -c "mysql -e 'DROP DATABASE IF EXISTS rubikvn';"
+docker-compose exec db bin/bash -c "mysql -e 'CREATE DATABASE rubikvn;'"
+
+sleep 1
+
+docker-compose exec db bin/bash -c "mysql -u root --default-character-set=utf8mb4 rubikvn < /root/rubikvn_schema.sql"
+
+docker-compose run web python manage.py migrate
