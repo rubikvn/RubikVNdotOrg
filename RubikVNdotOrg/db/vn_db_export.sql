0c27b00b038cfe044c46b092b886ea9290c13761
trungams <vttrung12@gmail.com>
2018-12-08 15:54:46 -0500

    Rewrite import script
    Since the tables have already been created right after the containers,
    removing and readding some of the tables that are referred to by tables
    managed by Django can cause errors. So it is easier to clear those tables
    instead of dropping them.
    Populate the WCA tables after clearing

--- RubikVNdotOrg/db/vn_db_export.sql
+++ RubikVNdotOrg/db/vn_db_export.sql
@@ -6,192 +6,149 @@ use rubikvn;
 -- Temporarily disable foreign key check for faster execution
 SET FOREIGN_KEY_CHECKS=0;
 
--- Creates tables in the db
--- Competitions
-DROP TABLE IF EXISTS Competitions;
-DROP TABLE IF EXISTS Continents;
-DROP TABLE IF EXISTS Countries;
-DROP TABLE IF EXISTS Events;
-DROP TABLE IF EXISTS Formats;
-DROP TABLE IF EXISTS Persons;
-DROP TABLE IF EXISTS Results;
-DROP TABLE IF EXISTS RanksAverage;
-DROP TABLE IF EXISTS RanksSingle;
-DROP TABLE IF EXISTS RoundTypes;
-DROP TABLE IF EXISTS Rounds;
-DROP TABLE IF EXISTS Scrambles;
-DROP TABLE IF EXISTS championships;
-DROP TABLE IF EXISTS eligible_country_iso2s_for_championship;
+-- Clear all WCA tables in the db
+TRUNCATE Competitions;
+TRUNCATE Continents;
+TRUNCATE Countries;
+TRUNCATE Events;
+TRUNCATE Formats;
+TRUNCATE Persons;
+TRUNCATE Results;
+TRUNCATE RanksAverage;
+TRUNCATE RanksSingle;
+TRUNCATE RoundTypes;
+TRUNCATE Rounds;
+TRUNCATE Scrambles;
+TRUNCATE championships;
+TRUNCATE eligible_country_iso2s_for_championship;
 
-CREATE TABLE IF NOT EXISTS Competitions AS
+-- Competitions
+INSERT INTO Competitions
     SELECT  *
     FROM wca.Competitions;
 
-ALTER TABLE Competitions
-ADD PRIMARY KEY (`id`);
-
 -- Continents
-CREATE TABLE IF NOT EXISTS Continents AS
+INSERT INTO Continents
     SELECT *
     FROM wca.Continents;
 
-ALTER TABLE Continents
-ADD PRIMARY KEY (`id`);
-
 -- Countries
-CREATE TABLE IF NOT EXISTS Countries AS
+INSERT INTO Countries
     SELECT *
     FROM  wca.Countries;
 
-ALTER TABLE Countries
-ADD PRIMARY KEY (`id`);
-
 -- Events
-CREATE TABLE IF NOT EXISTS Events AS
+INSERT INTO Events
     SELECT *
     FROM wca.Events;
 
-ALTER TABLE Events
-ADD PRIMARY KEY (`id`);
-
 -- Formats
-CREATE TABLE IF NOT EXISTS Formats AS
+INSERT INTO Formats
     SELECT *
     FROM wca.Formats;
 
-ALTER TABLE Formats
-ADD PRIMARY KEY (`id`);
-
-/*
- * Since we may be using sensitive information in this table (i.e emails, date of birth), a better way for extracting info from the WCA export would be to create a new table from the database export, and then join with the existing table in the db so that only new competitors are added into this table.
- */
-
--- or maybe don't put sensitive info on wca related db at all? And we can use WCA.org api for other functions.
 -- Persons
-CREATE TABLE IF NOT EXISTS Persons AS
+INSERT INTO Persons
     SELECT *
     FROM wca.Persons
     WHERE countryId = 'Vietnam';
 
-ALTER TABLE Persons
-ADD PRIMARY KEY (`id`);
-
 -- Results
-CREATE TABLE IF NOT EXISTS Results AS
+INSERT INTO Results (
+        competitionId,
+        eventId,
+        roundTypeId,
+        pos,
+        best,
+        average,
+        personName,
+        personId,
+        personCountryId,
+        formatId,
+        value1,
+        value2,
+        value3,
+        value4,
+        value5,
+        regionalSingleRecord,
+        regionalAverageRecord
+    )
     SELECT *
     FROM wca.Results
     WHERE personCountryId = 'Vietnam';
 
-ALTER TABLE Results
-ADD `id` int(10) PRIMARY KEY AUTO_INCREMENT;
-
 -- RoundTypes
-CREATE TABLE IF NOT EXISTS RoundTypes AS
+INSERT INTO RoundTypes
     SELECT *
     FROM wca.RoundTypes;
 
-ALTER TABLE RoundTypes
-ADD PRIMARY KEY (`id`);
-
 -- Rounds
-CREATE TABLE IF NOT EXISTS Rounds AS
+INSERT INTO Rounds (
+        sorry_message
+    )
     SELECT *
     FROM wca.Rounds;
 
-ALTER TABLE Rounds
-ADD `id` int(6) PRIMARY KEY AUTO_INCREMENT;
-
 -- Scrambles
-CREATE TABLE IF NOT EXISTS Scrambles
-    LIKE wca.Scrambles;
-
-ALTER TABLE Scrambles
-ADD PRIMARY KEY (`scrambleId`);
+INSERT INTO Scrambles
+    SELECT *
+    FROM wca.Scrambles;
 
 -- championships
-CREATE TABLE IF NOT EXISTS championships AS
+INSERT INTO championships
     SELECT *
     FROM wca.championships;
 
-ALTER TABLE championships
-ADD PRIMARY KEY (`id`);
-
 -- eligible_country_iso2s_for_championship
-CREATE TABLE IF NOT EXISTS eligible_country_iso2s_for_championship AS
+INSERT INTO eligible_country_iso2s_for_championship
     SELECT *
     FROM wca.eligible_country_iso2s_for_championship;
 
-ALTER TABLE eligible_country_iso2s_for_championship
-ADD PRIMARY KEY (`id`);
-
--- Add foreign keys
-ALTER TABLE Competitions
-ADD FOREIGN KEY (countryId) REFERENCES Countries(id);
-
-ALTER TABLE Countries
-ADD FOREIGN KEY (continentId) REFERENCES Continents(id);
-
-ALTER TABLE Persons
-ADD FOREIGN KEY (countryId) REFERENCES Countries(id);
-
-ALTER TABLE Results
-ADD FOREIGN KEY (competitionId) REFERENCES Competitions(id),
-ADD FOREIGN KEY (eventId) REFERENCES Events(id),
-ADD FOREIGN KEY (roundTypeId) REFERENCES RoundTypes(id),
-ADD FOREIGN KEY (personId) REFERENCES Persons(id),
-ADD FOREIGN KEY (personCountryId) REFERENCES Countries(id),
-ADD FOREIGN KEY (formatId) REFERENCES Formats(id);
-
-ALTER TABLE Scrambles
-ADD FOREIGN KEY (competitionId) REFERENCES Competitions(id),
-ADD FOREIGN KEY (eventId) REFERENCES Events(id),
-ADD FOREIGN KEY (roundTypeId) REFERENCES RoundTypes(id);
-
 -- RanksAverage
-CREATE TABLE IF NOT EXISTS RanksAverage AS
-SELECT  a.personId,
-a.eventId,
-a.best,
-a.worldRank,
-a.continentRank,
-a.countryRank,
-b.competitionId
-FROM wca.RanksAverage a
-INNER JOIN rubikvn.Results b
-ON a.personId = b.personId
-AND a.best = b.average
-GROUP BY b.personId, b.average;
+INSERT INTO RanksAverage (
+        personId,
+        eventId,
+        best,
+        worldRank,
+        continentRank,
+        countryRank,
+        competitionId
+    )
+    SELECT  a.personId,
+    a.eventId,
+    a.best,
+    a.worldRank,
+    a.continentRank,
+    a.countryRank,
+    b.competitionId
+    FROM wca.RanksAverage a
+    INNER JOIN rubikvn.Results b
+    ON a.personId = b.personId
+    AND a.best = b.average
+    GROUP BY b.personId, b.average;
 
 -- RanksSingle
-CREATE TABLE IF NOT EXISTS RanksSingle AS
-SELECT  a.personId,
-a.eventId,
-a.best,
-a.worldRank,
-a.continentRank,
-a.countryRank,
-b.competitionId
-FROM wca.RanksSingle a
-INNER JOIN rubikvn.Results b
-ON a.personId = b.personId
-AND a.best = b.best
-GROUP BY b.personId, b.best;
-
-ALTER TABLE RanksAverage
-ADD `id` int(6) PRIMARY KEY AUTO_INCREMENT;
-
-ALTER TABLE RanksSingle
-ADD `id` int(6) PRIMARY KEY AUTO_INCREMENT;
-
-ALTER TABLE RanksAverage
-ADD FOREIGN KEY (personId) REFERENCES Persons(id),
-ADD FOREIGN KEY (eventId) REFERENCES Events(id),
-ADD FOREIGN KEY (competitionId) REFERENCES Competitions(id);
-
-ALTER TABLE RanksSingle
-ADD FOREIGN KEY (personId) REFERENCES Persons(id),
-ADD FOREIGN KEY (eventId) REFERENCES Events(id),
-ADD FOREIGN KEY (competitionId) REFERENCES Competitions(id);
+INSERT INTO RanksSingle (
+        personId,
+        eventId,
+        best,
+        worldRank,
+        continentRank,
+        countryRank,
+        competitionId
+    )
+    SELECT  a.personId,
+    a.eventId,
+    a.best,
+    a.worldRank,
+    a.continentRank,
+    a.countryRank,
+    b.competitionId
+    FROM wca.RanksSingle a
+    INNER JOIN rubikvn.Results b
+    ON a.personId = b.personId
+    AND a.best = b.best
+    GROUP BY b.personId, b.best;
 
 -- Re-enable foreign key check before exit
 SET FOREIGN_KEY_CHECKS=1;
